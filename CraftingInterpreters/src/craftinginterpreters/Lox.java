6534f86b3b664aef6f75e5c349e00c033d83204c
Vidumini <heartyvds@gmail.com>
2019-04-20 17:13:57 +0530

    added method for running the prompt

--- CraftingInterpreters/src/craftinginterpreters/Lox.java
+++ CraftingInterpreters/src/craftinginterpreters/Lox.java
@@ -32,5 +32,16 @@ public class Lox {
     {
         byte[] bytes = Files.readAllBytes(Paths.get(path));
         run(new String(bytes, Charset.defaultCharset()));
+    }
+
+    private static void runPrompt() throws IOException
+    {
+        InputStreamReader input = new InputStreamReader(System.in);
+        BufferedReader reader = new BufferedReader(input);
+        
+        for(;;) {
+            System.out.print("> ");
+            run(reader.readLine());
+        }
     }    
 }
