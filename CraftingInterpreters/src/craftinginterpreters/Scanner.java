edfb0245bf9029c0fadb53de46a27c1ac1b18a79
Vidumini <heartyvds@gmail.com>
2019-04-20 17:08:32 +0530

    setup Scanner class

--- CraftingInterpreters/src/craftinginterpreters/Scanner.java
+++ CraftingInterpreters/src/craftinginterpreters/Scanner.java
@@ -5,10 +5,24 @@
  */
 package craftinginterpreters;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.HashMap;
+import java.util.Map;
+
 /**
  *
  * @author Vidumini
  */
-public class Scanner {
-    
+public class Scanner
+{
+    private final String source;
+    private final List<Token> tokens = new ArrayList<>();
+    private int start = 0;
+    private int current = 0;
+    private int line = 1;
+
+    public Scanner(String source) {
+        this.source = source;
+    }    
 }
