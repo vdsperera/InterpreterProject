3dc95c2483b45e5ed6ba44c212b18cade08a51a1
Vidumini <heartyvds@gmail.com>
2019-04-20 17:06:27 +0530

    added enums

--- CraftingInterpreters/src/craftinginterpreters/TokenType.java
+++ CraftingInterpreters/src/craftinginterpreters/TokenType.java
@@ -9,6 +9,24 @@ package craftinginterpreters;
  *
  * @author Vidumini
  */
-public enum TokenType {
-    
+public enum TokenType
+{
+  // Single-character tokens.                      
+  LEFT_PAREN, RIGHT_PAREN, LEFT_BRACE, RIGHT_BRACE,
+  COMMA, DOT, MINUS, PLUS, SEMICOLON, SLASH, STAR, 
+
+  // One or two character tokens.                  
+  BANG, BANG_EQUAL,                                
+  EQUAL, EQUAL_EQUAL,                              
+  GREATER, GREATER_EQUAL,                          
+  LESS, LESS_EQUAL,                                
+
+  // Literals.                                     
+  IDENTIFIER, STRING, NUMBER,                      
+
+  // Keywords.                                     
+  AND, CLASS, ELSE, FALSE, FUN, FOR, IF, NIL, OR,  
+  PRINT, RETURN, SUPER, THIS, TRUE, VAR, WHILE,    
+
+  EOF        
 }
