c851f65370ebaf887436db3cee445a058311c10d
Vidumini <heartyvds@gmail.com>
2019-04-20 17:15:25 +0530

    added method for outputting error

--- CraftingInterpreters/src/craftinginterpreters/Lox.java
+++ CraftingInterpreters/src/craftinginterpreters/Lox.java
@@ -53,5 +53,10 @@ public class Lox {
         for (Token token : tokens) {
             System.out.println(token);
         }
+    }
+
+    static void error(int line, String message)
+    {
+        report(line, "", message);
     }    
 }
