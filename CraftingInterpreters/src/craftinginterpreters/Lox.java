776459c2c00e29a796f43b517b0d54a2e01e0a15
Vidumini <heartyvds@gmail.com>
2019-04-20 17:13:30 +0530

    added method for running the file

--- CraftingInterpreters/src/craftinginterpreters/Lox.java
+++ CraftingInterpreters/src/craftinginterpreters/Lox.java
@@ -5,6 +5,16 @@
  */
 package craftinginterpreters;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.nio.charset.Charset;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.List;
+import craftinginterpreters.Token;
+import craftinginterpreters.Scanner;
+
 /**
  *
  * @author Vidumini
@@ -18,4 +28,9 @@ public class Lox {
         // TODO code application logic here
     }
     
+    private static void runFile(String path) throws IOException
+    {
+        byte[] bytes = Files.readAllBytes(Paths.get(path));
+        run(new String(bytes, Charset.defaultCharset()));
+    }    
 }
