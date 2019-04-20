6d697b6cfa201d75318fee0da18d1b66e713fb55
Vidumini <heartyvds@gmail.com>
2019-04-20 17:15:51 +0530

    added method for reporting error

--- CraftingInterpreters/src/craftinginterpreters/Lox.java
+++ CraftingInterpreters/src/craftinginterpreters/Lox.java
@@ -58,5 +58,12 @@ public class Lox {
     static void error(int line, String message)
     {
         report(line, "", message);
+    }
+
+    private static void report(int line, String where, String message) 
+    {
+        System.err.println(
+         "[line " + line + "]" + where + ": " + message);
+        hadError = true;
     }    
 }
