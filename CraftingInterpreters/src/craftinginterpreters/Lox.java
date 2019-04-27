2e8b814770bdeac404f18b9356f67652fd654350
Vidumini <heartyvds@gmail.com>
2019-04-27 15:34:07 +0530

    set main method for run file and prompt

--- CraftingInterpreters/src/craftinginterpreters/Lox.java
+++ CraftingInterpreters/src/craftinginterpreters/Lox.java
@@ -25,8 +25,16 @@ public class Lox {
     /**
      * @param args the command line arguments
      */
-    public static void main(String[] args) {
-        // TODO code application logic here
+    public static void main(String[] args) throws IOException
+    {
+        if (args.length > 1) {                                   
+          System.out.println("Usage: jlox [script]");            
+          System.exit(64); 
+        } else if (args.length == 1) {                           
+          runFile(args[0]);                                      
+        } else {                                                 
+          runPrompt();                                           
+        } 
     }
     
     private static void runFile(String path) throws IOException
